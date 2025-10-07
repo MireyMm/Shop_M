c7ad5dfc0a353bc7dabe8e2f0356a735d2511cfb
MireyMm <mizuizu77@gmail.com>
2025-10-07 12:00:27 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+const tombolSapa = document.getElementByid('sapaButton');
+tombolSapa.addEventListener('click',function(){
+  alert('Halo! Terima kasih sudah mengunjungi M Shop 😄'):
+});
+document.getElementById("modeToggle").addEventListener("click", () => {
+  document.body.classList.toggle("gelap");
+});
